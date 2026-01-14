2c5b9664d805e0818f323ba696498347b2d082bd
Saintt1309 <farrelirfandri@gmail.com>
2026-01-14 20:25:32 +0700

    fixes

---
+++ experiential design project/Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QWJKYkVDSC8vLy8vQUFBQm1YeXVOekJ4QzBzQ25aOWV5VUdwaTJOZlVielU3NEhoeUNrSDB3VlFDSzl1cHlLYkEwWm45RmEyVk5qV3BGOGVObnhGMDE2Y3h6bjBpOUpJV0J0Y2E5ZWtWN2hNRTlkS0ZzbjRYUDVNR2xieS9wMzZwcGJTdjgyZHVETVBROVhzV1FUbHhkYzltd1dUNHMwRXdFUW1pT04yUUhOcnZmdTl4VHU4QTBUeDZLNG5pbnY3OFpBbHNWSW1kQWlNUFZZaDFPcDhYcmRtaWU0SGVIajBBcTZNZUlzNThhdkhmcXFuRlFqMW1zYmZkdWlaOFlETitwNFFrc1hiSVJmZGRvckdRa1JzK0oxdHZCeUJ4dVpGczJCNDFUZnkxUXZVdGNSMlBxZm12RmZsSDVkckNFeVNZdlFmWmJHMk16SzVqTlBwTEJCWW9XbmxXKytTZTdmSWVxdXliNUZ6OU14WHJYeFh6NTRmSUh0U085aUQ=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
